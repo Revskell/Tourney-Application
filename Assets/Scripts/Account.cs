ea36347a0808d157625c53e35f848cf7b814c52f
@JRC04 <joeljrc04@gmail.com>
2023-05-30 16:50:20 +0200

    Fixing up some security flaws

---
+++ Assets/Scripts/Account.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Account
+{
+    public string _id;
+    public string username;
+}
--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -3,22 +3,33 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System;
 
 public class Login : MonoBehaviour
 {
-    [SerializeField] private string authenticationEndpoint = "http://127.0.0.1:13756/account";
+    [SerializeField] private string loginEndpoint = "http://127.0.0.1:13756/account/login";
+    [SerializeField] private string createEndpoint = "http://127.0.0.1:13756/account/create";
 
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private TMP_InputField passwordInputField;
     [SerializeField] private TextMeshProUGUI alertText;
     [SerializeField] private Button loginButton;
+    [SerializeField] private Button registerButton;
 
     public void OnLoginClick()
     {
         alertText.text = "Signing in...";
-        loginButton.interactable = false;
+        ActivateButtons(false);
 
         StartCoroutine(TryLogin());
+    }
+
+    public void OnRegisterClick()
+    {
+        alertText.text = "Creating a new account...";
+        ActivateButtons(false);
+
+        StartCoroutine(TryCreate());
     }
 
     private IEnumerator TryLogin()
@@ -40,7 +51,11 @@ public class Login : MonoBehaviour
             yield break;
         }
 
-        UnityWebRequest request = UnityWebRequest.Get($"{authenticationEndpoint}?rUsername={username}&rPassword={password}");
+        WWWForm form = new WWWForm();
+        form.AddField("rUsername", username);
+        form.AddField("rPassword", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(loginEndpoint, form);
         var handler = request.SendWebRequest();
 
 
@@ -56,24 +71,84 @@ public class Login : MonoBehaviour
         {
             if(!request.downloadHandler.text.Equals("Invalid Credentials")) // login success
             {
-                alertText.text = "Welcome";
-                loginButton.interactable = false;
+                ActivateButtons(false);
+                Account returnedAccount = JsonUtility.FromJson<Account>(request.downloadHandler.text); // retrieve data
+                alertText.text = "Welcome" + returnedAccount.username;
             }
             else
             {
                 alertText.text = "Invalid Credentials";
-                loginButton.interactable = true;
+                ActivateButtons(true);
             }
 
             Debug.Log(request.downloadHandler.text);
         }
         else
         {
-            loginButton.interactable = true;
+            ActivateButtons(true);
             alertText.text = "Error connecting to the server...";
         }
 
         yield return null;
     }
+
+    private IEnumerator TryCreate()
+    {
+        string username = usernameInputField.text;
+        string password = passwordInputField.text;
+
+        if (username.Length < 3 || username.Length > 24)
+        {
+            alertText.text = "Invalid username";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        if (password.Length < 3 || password.Length > 24)
+        {
+            alertText.text = "Invalid password";
+            ActivateButtons(true);
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("rUsername", username);
+        form.AddField("rPassword", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(createEndpoint, form);
+        var handler = request.SendWebRequest();
+
+
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+            if (startTime > 10.0f) break;
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            if (!request.downloadHandler.text.Equals("Invalid Credentials") && !request.downloadHandler.text.Equals("Username is already taken")) // login success
+            {
+                Account returnedAccount = JsonUtility.FromJson<Account>(request.downloadHandler.text); // retrieve data
+                alertText.text = "Account has been created";
+            }
+            else alertText.text = "Username is already taken";
+
+            Debug.Log(request.downloadHandler.text);
+        }
+        else alertText.text = "Error connecting to the server...";
+
+        ActivateButtons(true);
+
+        yield return null;
+    }
+
+    private void ActivateButtons(bool toggle)
+    {
+        loginButton.interactable = toggle;
+        registerButton.interactable = toggle;
+    } 
 
 }
